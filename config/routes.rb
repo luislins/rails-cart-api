e100a2a0406c839b209303c798390f105edd2e71
Luis Guilherme <luisguilherme.siqueira32@gmail.com>
2026-03-27 12:28:19 -0300

    add tests and cart routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
+
+  resource :cart, only: [:show, :create] do
+    post :add_item
+    delete ':product_id', action: :remove_item, on: :member
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "rails/health#show"
--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when validating' do
+    it 'is valid with valid attributes' do
+      cart_item = build(:cart_item)
+      expect(cart_item).to be_valid
+    end
+
+    it 'validates numericality of quantity' do
+      cart_item = build(:cart_item, quantity: -1)
+      expect(cart_item.valid?).to be_falsey
+      expect(cart_item.errors[:quantity]).to include("must be greater than 0")
+    end
+  end
+
+  context 'associations' do
+    it { expect(described_class.reflect_on_association(:cart).macro).to eq(:belongs_to) }
+    it { expect(described_class.reflect_on_association(:product).macro).to eq(:belongs_to) }
+  end
 end
