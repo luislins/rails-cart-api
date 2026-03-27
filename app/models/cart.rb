2c639f64ed5387b71da3c741a368f63d28abb907
Luis Guilherme <luisguilherme.siqueira32@gmail.com>
2026-03-27 12:23:28 -0300

    create cart item model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,22 @@
 class Cart < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+  has_many :products, through: :cart_items
+
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
-  # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
+  def mark_as_abandoned
+    update(abandoned: true) if last_interaction_at <= 3.hours.ago
+  end
+
+  def remove_if_abandoned
+    destroy if abandoned? && last_interaction_at <= 7.days.ago
+  end
+
+  def update_total_price
+    update(total_price: cart_items.joins(:product).sum('products.price * cart_items.quantity'))
+  end
+
+  def touch_interaction
+    update(last_interaction_at: Time.current)
+  end
 end
--- db/migrate/20260327140243_create_cart_items.rb
+++ db/migrate/20260327140243_create_cart_items.rb
@@ -1,4 +1,3 @@
-
 class CreateCartItems < ActiveRecord::Migration[7.1]
   def change
     create_table :cart_items do |t|
