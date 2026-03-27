35abbbc249e2f73447b51e1fed5655613a1acee6
Luis Guilherme <luisguilherme.siqueira32@gmail.com>
2026-03-27 14:13:55 -0300

    adding card abandoned job

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -1,7 +1,22 @@
 class MarkCartAsAbandonedJob
   include Sidekiq::Job
 
-  def perform(*args)
-    # TODO Impletemente um Job para gerenciar, marcar como abandonado. E remover carrinhos sem interação. 
+  def perform
+    mark_abandoned_carts
+    remove_old_abandoned_carts
+  end
+
+  private
+
+  def mark_abandoned_carts
+    Cart.where(abandoned: false)
+        .where('last_interaction_at <= ?', 3.hours.ago)
+        .find_each(&:mark_as_abandoned)
+  end
+
+  def remove_old_abandoned_carts
+    Cart.where(abandoned: true)
+        .where('last_interaction_at <= ?', 7.days.ago)
+        .find_each(&:remove_if_abandoned)
   end
 end
--- spec/sidekiq/mark_cart_as_abandoned_job_spec.rb
+++ spec/sidekiq/mark_cart_as_abandoned_job_spec.rb
@@ -1,3 +1,37 @@
 require 'rails_helper'
+
 RSpec.describe MarkCartAsAbandonedJob, type: :job do
+  describe '#perform' do
+    context 'marking carts as abandoned' do
+      it 'marks carts inactive for more than 3 hours as abandoned' do
+        cart = create(:shopping_cart, last_interaction_at: 4.hours.ago)
+
+        described_class.new.perform
+
+        expect(cart.reload.abandoned?).to be true
+      end
+
+      it 'does not mark carts with recent interaction as abandoned' do
+        cart = create(:shopping_cart, last_interaction_at: 1.hour.ago)
+
+        described_class.new.perform
+
+        expect(cart.reload.abandoned?).to be false
+      end
+    end
+
+    context 'removing old abandoned carts' do
+      it 'removes carts abandoned for more than 7 days' do
+        cart = create(:shopping_cart, abandoned: true, last_interaction_at: 8.days.ago)
+
+        expect { described_class.new.perform }.to change { Cart.count }.by(-1)
+      end
+
+      it 'does not remove recently abandoned carts' do
+        cart = create(:shopping_cart, abandoned: true, last_interaction_at: 2.days.ago)
+
+        expect { described_class.new.perform }.not_to change { Cart.count }
+      end
+    end
+  end
 end
